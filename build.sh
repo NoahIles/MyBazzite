68c484b1cd282a55e7f6853a503979947ea4a10f
Noah Iles <noahiles@gmail.com>
2024-11-02 07:01:22 -0700

    Use brew to install shell tools if able

--- build.sh
+++ build.sh
@@ -28,12 +28,12 @@ shell_tools=(
 
 
 # Install Homebrew if it's not already installed
-# if ! command -v brew &> /dev/null; then
-#     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-# fi
+if command -v brew &> /dev/null; then
+    brew update
+    brew install ${shell_tools[@]}
+    # /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
 
-# # Update Homebrew
-# brew update
+# Update Homebrew
 
-# brew install ${shell_tools[@]}
 # rpm-ostree install ${packages[@]}
